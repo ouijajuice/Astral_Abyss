be6e4473d0f98f55a2ec8cc0681336a4377ae56d
ouijajuice <eearls@ltu.edu>
2024-02-12 13:23:06 -0500

    kill counter and new font

---
+++ Assets/Scripts/KillCounter.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KillCounter : MonoBehaviour
+{
+    public int killCount;
+    public TextMeshProUGUI killText;
+    // Update is called once per frame
+    void Update()
+    {
+        killText.text = killCount.ToString();
+    }
+}
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,6 +6,11 @@ public class Projectile : MonoBehaviour
 {
     public GameObject playerExplosion;
     public string hitTag;
+    private GameObject killCounterScript;
+    private void Start()
+    {
+        
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag(hitTag))
@@ -13,6 +18,12 @@ public class Projectile : MonoBehaviour
             GameObject explosion = Instantiate(playerExplosion, transform.position, Quaternion.identity);
             Destroy(explosion, 1f);
             Destroy(gameObject);
+            if (hitTag == "Enemy")
+            {
+                GameObject killCounter = GameObject.FindGameObjectWithTag("KillCounterObject");
+                KillCounter killCounterScript = killCounter.GetComponent<KillCounter>();
+                killCounterScript.killCount -= 1;
+            }
         }
         Destroy(gameObject,5f);
     }
